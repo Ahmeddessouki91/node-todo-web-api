40a997b6db020fd07afd025a9a17d5f15fef0e09
ahmed.dessouki91 <eng.ahmed.dessouki91@gmail.com>
2018-03-14 18:30:07 +0200

    add POST /users/login route

--- models/user.js
+++ models/user.js
@@ -63,6 +63,23 @@ UserSchema.statics.findByToken = function (token) {
     return User.findOne({ '_id': decoded._id, 'tokens.token': token, 'tokens.access': 'auth' });
 }
 
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user)
+            return Promise.reject();
+
+        return new Promise((resolve, reject) => {
+            bycrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                }
+                reject();
+            });
+        });
+    });
+}
+
 UserSchema.pre('save', function (next) {
     var user = this;
 
